d50aa8677ab55e769e6d3dc35fa4efa6cc15b6d8
ProXyTG <proxytg1@gmail.com>
2021-05-30 22:36:33 +0200

    add footer

---
+++ components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <footer>
+    <div class="main-footer">
+      <img src="@/assets/images/footer/Notus-logo-footer.svg" />
+    </div>
+    <div class="floating-footer">
+      <div class="copyright-notus">
+        <p>&#169; {{ new Date().getFullYear() }}. Notus IT d.o.o.</p>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss">
+footer {
+  position: relative;
+
+  .main-footer {
+    min-height: 200px;
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .floating-footer {
+    background-color: $secondary-text;
+    max-width: 1200px;
+    width: 100%;
+    min-height: 100px;
+    position: absolute;
+    top: 0;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    p {
+      color: #fff;
+      transform: matrix(1, 0, 0, 1, 0, 0);
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
     <Nuxt />
+    <Footer />
   </div>
 </template>
 
 <script>
+import Footer from '@/components/Footer.vue'
 
 export default {
-
+  components: {
+    Footer
+  }
 }
 </script>
 
